4a3d318a7a90f869832fbd0b2aed288f694d03a1
Michael Dunn <dunnmj42@gmail.com>
2021-02-25 10:44:13 -0600

    database update

--- database.sql
+++ database.sql
@@ -7,7 +7,7 @@ CREATE TABLE "owners"(
 
 CREATE TABLE "pets"(
     "id" SERIAL PRIMARY KEY,
-    "owner_id" INT REFERENCES "owners",
+    "owner_id" INT REFERENCES "owners" ON DELETE CASCADE,
     "pet_name" varchar,
     "breed" VARCHAR(100),
     "color" VARCHAR(100),
@@ -24,7 +24,7 @@ INSERT INTO "pets" ("owner_id", "pet_name", "breed", "color")
 VALUES (1, 'Roux', 'Golden Lab mix', 'Golden'), (3, 'Carl', 'Pomeranian', 'White');
 
 DROP TABLE "owners";
-DROP TABLE "pets";
+DROP TAner_id GROUP BY "owners".id, "pets".id;BLE "pets";
 
 
-SELECT "owners".*, COUNT("pets".id) AS "pet_count" FROM "owners" LEFT JOIN "pets" ON "owners".id = "pets".owner_id GROUP BY "owners".id, "pets".id;
+SELECT "owners".*, COUNT("pets".id) AS "pet_count" FROM "owners" LEFT JOIN "pets" ON "owners".id = "pets".ow
